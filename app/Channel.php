1949c3be8d281a70b34e6550d08200bcde447547
vagrant <=>
2018-02-13 12:00:13 +0000

    Changes to app/Channel.php

--- app/Channel.php
+++ app/Channel.php
@@ -6,13 +6,66 @@ use Illuminate\Database\Eloquent\Model;
 
 class Channel extends Model
 {
-    public function getRouteKeyName()
+    /**
+     * The attributes that aren't mass assignable.
+     *
+     * @var array
+     */
+    protected $guarded = [];
+
+    /**
+     * Attributes to cast.
+     */
+    protected $casts = [
+        'archived' => 'boolean'
+    ];
+
+    protected static function boot()
     {
-    	return 'slug';
+        parent::boot();
+
+        static::addGlobalScope('active', function ($builder) {
+            $builder->where('archived', false)
+                ->orderBy('name', 'asc');
+        });
     }
 
+    /**
+     * Get the route key name for Laravel.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
+    /**
+     * A channel consists of threads.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
     public function threads()
     {
-    	return $this->hasMany(Thread::class);
+        return $this->hasMany(Thread::class);
+    }
+
+    /**
+     * Archive the channel.
+     */
+    public function archive()
+    {
+        $this->update(['archived' => true]);
+    }
+
+    /**
+     * Set the name of the channel.
+     *
+     * @param string $name
+     */
+    public function setNameAttribute($name)
+    {
+        $this->attributes['name'] = $name;
+        $this->attributes['slug'] = str_slug($name);
     }
 }
