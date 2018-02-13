ae40340f85ccc4cea08ef5ecdb408752b2d561ad
vagrant <=>
2018-02-13 12:19:32 +0000

    Fixes

--- app/Channel.php
+++ app/Channel.php
@@ -7,30 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 class Channel extends Model
 {
     /**
-     * The attributes that aren't mass assignable.
-     *
-     * @var array
-     */
-    protected $guarded = [];
-
-    /**
-     * Attributes to cast.
-     */
-    protected $casts = [
-        'archived' => 'boolean'
-    ];
-
-    protected static function boot()
-    {
-        parent::boot();
-
-        static::addGlobalScope('active', function ($builder) {
-            $builder->where('archived', false)
-                ->orderBy('name', 'asc');
-        });
-    }
-
-    /**
      * Get the route key name for Laravel.
      *
      * @return string
@@ -48,24 +24,5 @@ class Channel extends Model
     public function threads()
     {
         return $this->hasMany(Thread::class);
-    }
-
-    /**
-     * Archive the channel.
-     */
-    public function archive()
-    {
-        $this->update(['archived' => true]);
-    }
-
-    /**
-     * Set the name of the channel.
-     *
-     * @param string $name
-     */
-    public function setNameAttribute($name)
-    {
-        $this->attributes['name'] = $name;
-        $this->attributes['slug'] = str_slug($name);
     }
 }
